9bc060c63c2de3747ff2a7f48640378d0e9232d1
Daniel Morgan <themorgantown@gmail.com>
2013-01-22 16:59:15 -0800

    video examples

---
+++ widgets/video/HypeVideoSelfContained.wdgt/HypeVideoSelfContained.wdgt.resources/hypevideoselfcontained_hype_generated_script.js
@@ -0,0 +1,113 @@
+//	HYPE.documents["HypeVideoSelfContained.wdgt"]
+
+(function HYPE_DocumentLoader() {
+	var resourcesFolderName = "HypeVideoSelfContained.wdgt.resources";
+	var documentName = "HypeVideoSelfContained.wdgt";
+	var documentLoaderFilename = "hypevideoselfcontained_hype_generated_script.js";
+	var mainContainerID = "hypevideoselfcontained_hype_container";
+
+	// find the URL for this script's absolute path and set as the resourceFolderName
+	try {
+		var scripts = document.getElementsByTagName('script');
+		for(var i = 0; i < scripts.length; i++) {
+			var scriptSrc = scripts[i].src;
+			if(scriptSrc != null && scriptSrc.indexOf(documentLoaderFilename) != -1) {
+				resourcesFolderName = scriptSrc.substr(0, scriptSrc.lastIndexOf("/"));
+				break;
+			}
+		}
+	} catch(err) {	}
+
+	// Legacy support
+	if (typeof window.HYPE_DocumentsToLoad == "undefined") {
+		window.HYPE_DocumentsToLoad = new Array();
+	}
+ 
+	// load HYPE.js if it hasn't been loaded yet
+	if(typeof HYPE_150 == "undefined") {
+		if(typeof window.HYPE_150_DocumentsToLoad == "undefined") {
+			window.HYPE_150_DocumentsToLoad = new Array();
+			window.HYPE_150_DocumentsToLoad.push(HYPE_DocumentLoader);
+
+			var headElement = document.getElementsByTagName('head')[0];
+			var scriptElement = document.createElement('script');
+			scriptElement.type= 'text/javascript';
+			scriptElement.src = resourcesFolderName + '/' + 'HYPE.js?hype_version=150';
+			headElement.appendChild(scriptElement);
+		} else {
+			window.HYPE_150_DocumentsToLoad.push(HYPE_DocumentLoader);
+		}
+		return;
+	}
+	
+	// handle attempting to load multiple times
+	if(HYPE.documents[documentName] != null) {
+		var index = 1;
+		var originalDocumentName = documentName;
+		do {
+			documentName = "" + originalDocumentName + "-" + (index++);
+		} while(HYPE.documents[documentName] != null);
+		
+		var allDivs = document.getElementsByTagName("div");
+		var foundEligibleContainer = false;
+		for(var i = 0; i < allDivs.length; i++) {
+			if(allDivs[i].id == mainContainerID && allDivs[i].getAttribute("HYPE_documentName") == null) {
+				var index = 1;
+				var originalMainContainerID = mainContainerID;
+				do {
+					mainContainerID = "" + originalMainContainerID + "-" + (index++);
+				} while(document.getElementById(mainContainerID) != null);
+				
+				allDivs[i].id = mainContainerID;
+				foundEligibleContainer = true;
+				break;
+			}
+		}
+		
+		if(foundEligibleContainer == false) {
+			return;
+		}
+	}
+	
+	var hypeDoc = new HYPE_150();
+	
+	var attributeTransformerMapping = {b:"i",c:"i",bC:"i",d:"i",aS:"i",M:"i",e:"f",N:"i",f:"d",aT:"i",O:"i",g:"c",aU:"i",P:"i",Q:"i",aV:"i",R:"c",bG:"f",aW:"f",aI:"i",S:"i",bH:"d",l:"d",aX:"i",aJ:"i",m:"c",bI:"f",T:"i",n:"c",aK:"i",bJ:"f",aZ:"i",X:"i",A:"c",bK:"f",Y:"bM",B:"c",aL:"i",bL:"f",C:"c",D:"c",t:"i",E:"i",G:"c",bA:"c",a:"i",bB:"i"};
+	
+	var resources = {"0":{n:"5.appletv.mp4"},"1":{n:"WhatsNew1_6.mp4"}};
+	
+	var scenes = [{x:0,p:"600px",c:"#FFFFFF",v:{"2":{aR:"0",x:"visible",bE:[{type:"video/mp4",filename:"0"}],a:0,b:9,j:"absolute",c:600,k:"video",aO:"1",d:375,z:"1",aQ:"0",aH:"1"}},n:"Untitled Scene",T:{kTimelineDefaultIdentifier:{d:599.25,i:"kTimelineDefaultIdentifier",n:"Main Timeline",a:[{t:0,p:1,i:"Video Track",d:599.25,o:"2",f:"2"}],f:30}},o:"1"}];
+	
+	var javascripts = [];
+	
+	var functions = {};
+	var javascriptMapping = {};
+	for(var i = 0; i < javascripts.length; i++) {
+		try {
+			javascriptMapping[javascripts[i].identifier] = javascripts[i].name;
+			eval("functions." + javascripts[i].name + " = " + javascripts[i].source);
+		} catch (e) {
+			hypeDoc.log(e);
+			functions[javascripts[i].name] = (function () {});
+		}
+	}
+	
+	hypeDoc.setAttributeTransformerMapping(attributeTransformerMapping);
+	hypeDoc.setResources(resources);
+	hypeDoc.setScenes(scenes);
+	hypeDoc.setJavascriptMapping(javascriptMapping);
+	hypeDoc.functions = functions;
+	hypeDoc.setCurrentSceneIndex(0);
+	hypeDoc.setMainContentContainerID(mainContainerID);
+	hypeDoc.setResourcesFolderName(resourcesFolderName);
+	hypeDoc.setShowHypeBuiltWatermark(0);
+	hypeDoc.setShowLoadingPage(false);
+	hypeDoc.setDrawSceneBackgrounds(true);
+	hypeDoc.setGraphicsAcceleration(true);
+	hypeDoc.setDocumentName(documentName);
+
+	HYPE.documents[documentName] = hypeDoc.API;
+	document.getElementById(mainContainerID).setAttribute("HYPE_documentName", documentName);
+
+	hypeDoc.documentLoad(this.body);
+}());
+
